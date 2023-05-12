19491fc4ae123a28d4d4430d44032c057fcfa2f2
Hector-Leonardo <132015462+Hector-Leonardo@users.noreply.github.com>
2023-05-11 18:09:16 -0600

    seccionDistrito.java

--- Clase.java
+++ Clase.java
@@ -1,67 +1,76 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Models;
 
-/**
- *
- * @author ADMIN
- */
-class Clase {
-    private int id;
-    private String nombre;
-    private double latitud;
-    private double longitud;
-    private int municipio;
+package secciondistrito;
+import Models.Seccion;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class SeccionDistritos {
+    static Scanner entradaDato= new Scanner (System.in);
+    static ArrayList<Seccion> secciones= new ArrayList<>();
+            
+public static void llenarValores() {
+        System.out.println("Listado total:");
+        int Listado = entradaDato.nextInt();
+        System.out.println("Votantes totales");
+        int Votantes = entradaDato.nextInt();
+        secciones.add(new Seccion(Listado,Votantes));
+        porcentajeParticipacion();
+    }
+
+public static void porcentajeParticipacion(){
+    int Votantes = secciones.get(0).getVotantes();
+    int Listado = secciones.get(0). getListado();
+    double operacion = ((double) Votantes / Listado)*100;
+    
+    System.out.println("Porcentaje de participacion: " + operacion);
+    porcentajeAbstinencia(Votantes, Listado);
+    }
+public static void porcentajeAbstinencia(int votantes, int listados){
+        int resto = listados-votantes;
+        double operacion = ((double) resto / listados) * 100;
+        System.out.println("Porcentaje de abstinencia: " + operacion);
+        faltantes(votantes, listados);
+    }
+public static void faltantes(int votantes, int listados) {
+        int operacion = (listados-votantes);
+        System.out.println("Faltantes: " + operacion);
+    }
+public static void main(String[] args) {
+        llenarValores();
+    }
+}
+
+package Models;
+public class Seccion {
     private int listado;
     private int votantes;
 
-    public Clase() {
+    public Seccion() {
     }
 
-    public Clase(int id, String nombre, double latitud, double longitud, int municipio, int listado, int votantes) {
-        this.id = id;
-        this.nombre = nombre;
-        this.latitud = latitud;
-        this.longitud = longitud;
-        this.municipio = municipio;
+    public Seccion(int listado, int votantes) {
         this.listado = listado;
         this.votantes = votantes;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public double getLatitud() {
-        return latitud;
-    }
-
-    public double getLongitud() {
-        return longitud;
-    }
-
-    public int getMunicipio() {
-        return municipio;
     }
 
     public int getListado() {
         return listado;
     }
 
+    public void setListado(int listado) {
+        this.listado = listado;
+    }
+
     public int getVotantes() {
         return votantes;
     }
 
+    public void setVotantes(int votantes) {
+        this.votantes = votantes;
+    }
+
     @Override
     public String toString() {
-        return "Clase{" + "id=" + id + ", nombre=" + nombre + ", latitud=" + latitud + ", longitud=" + longitud + ", municipio=" + municipio + ", listado=" + listado + ", votantes=" + votantes + '}';
+        return "Seccion{" + "listado=" + listado + ", votantes=" + votantes + '}';
     }
-    
-    
-}
+
+    }
